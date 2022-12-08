8283b856d3364c680de25727a2ba16ff45fc784e
KougakuinKamata <115198370+KougakuinKamata@users.noreply.github.com>
2022-12-08 11:15:39 +0900

    Add files via upload

---
+++ PG_11-1/Enemy.h
@@ -0,0 +1,34 @@
+#pragma once
+class Enemy
+{
+public:	// メンバ関数
+	
+	
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="posX">X座標</param>
+	/// <param name="posY">Y座標</param>
+	/// <param name="speedX">移動速度X</param>
+	/// <param name="speedY">移動速度Y</param>
+	Enemy(int posX, int posY, int speedX, int speedY);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+
+public:	// メンバ変数
+	int posX_;
+	int posY_;
+	int speedX_;
+	int speedY_;
+	int radius_;
+};
+
